666c116173f80cd58092544bb89f839dd5630f1c
Aladim Santana <aladimleite@gmail.com>
2023-11-28 08:26:32 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dscatalog.dto;
 
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+
 //20231126 aula 03-07 CRUD de usuários PARTE2
+
+@UserInsertValid //20231127 aula 03-13
 public class UserInsertDTO extends UserDTO {
     private static final long serialVersionUID = 1L;
 
--- backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dscatalog.entities.User;
+import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>  {
-    
+
+    //03-13 1524
+    User findByEmail(String email);
 }
