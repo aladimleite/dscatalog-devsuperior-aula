a06cd72c7c1d864c75ff085ea8a4c80599b980e3
Aladim Santana <aladimleite@gmail.com>
2023-11-29 17:11:04 -0300

    Resource Server

---
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dscatalog.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+//aula 03-26
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    private static final String[] PUBLIC = { "/oauth/token" };
+
+    private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
+
+    private static final String[] ADMIN = { "/users/**" }; 
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        //02m52s com isso aqui o Resource server (vide figura) será capaz de decodificar o token e verificar se é válido
+        resources.tokenStore(tokenStore);        
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+        .antMatchers(PUBLIC).permitAll()
+        .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
+        .antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN") //no banco é obrigatório o prefixo "ROLE". Ex.: ROLE_OPERATOR, ROLE_ADMIN
+        .antMatchers(ADMIN).hasRole("ADMIN")
+        .anyRequest().authenticated(); //para qualquer outra rota deve estar logado
+
+    }
+    
+    
+}
